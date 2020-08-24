6b24a000cee53832a704b7d2db886b8bd28a1a75
Rick0335 <Rick0335@edu.eal.dk>
2020-08-24 11:36:05 +0200

    Added the initial commander skeleton in 'index.js'

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 #!/usr/bin/env node
 
+const commander = require("commander");
+
+commander
+    .command("generate")
+    .description("generate a html file based on a resume.json file")
+    .action((args, options) => {
+        require("./actions/generate")(args, options);
+    })
+
+commander.parse(process.argv);
